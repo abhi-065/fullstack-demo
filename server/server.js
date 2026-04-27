548526508eae55a276a88884e1e9a44934819545
abhi-065 <abhiramdns@gmail.com>
2026-04-27 16:12:07 +0530

    fix db connection

--- server/server.js
+++ server/server.js
@@ -5,14 +5,29 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+console.log("ENV:", process.env.DB_HOST);
+// DB connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Abhi@065",
-  database: "tasksdb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
+// 🔥 IMPORTANT: Check connection
+db.connect((err) => {
+  if (err) {
+    console.error("DB connection failed ❌:", err);
+  } else {
+    console.log("Connected to DB ✅");
+  }
+});
+
+// Create task
 app.post("/add", (req, res) => {
   const { task } = req.body;
   db.query("INSERT INTO tasks (task) VALUES (?)", [task], (err) => {
@@ -21,6 +36,7 @@ app.post("/add", (req, res) => {
   });
 });
 
+// Get tasks
 app.get("/tasks", (req, res) => {
   db.query("SELECT * FROM tasks", (err, result) => {
     if (err) return res.send(err);
